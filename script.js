af6091e48eefa950a13e77f6e83f6ae1c4054958
Paula Portela <paulaportella@gmail.com>
2024-07-14 11:07:43 -0300

    logo

---
+++ script.js
@@ -0,0 +1,16 @@
+// document = ir no HTML
+// querySelector = busca o que eu quiser que ele busque
+
+let formulario = document.querySelector('.formulario')
+
+
+function aparecerFormulario() {
+  formulario.style.left = "50%"
+
+
+}
+
+function desaparecerFormulario() {
+    formulario.style.left = "-50%"
+  
+}
